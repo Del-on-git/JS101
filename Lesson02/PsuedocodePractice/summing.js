8ac760affd4ad53b7a0d7d2e086d8da6c8fa2fc9
Del-on-git <del@delizardo.com>
2021-04-03 15:04:04 -0500

    wrote program to sum two numbers

---
+++ Lesson02/PsuedocodePractice/summing.js
@@ -0,0 +1,11 @@
+//START
+let readline = require('readline-sync');
+//SET number1 to first value
+let number1 = parseFloat(readline.question("Number 1: "));
+//SET number2 to second value
+let number2 = parseFloat(readline.question("Number 2: "));
+//SET sum to number1 + number2
+let sum = number1 + number2;
+//PRINT sum
+console.log(sum);
+//END

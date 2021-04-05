9b7455bf62794c0c8c829bc1284c62cfba87a936
Del-on-git <del@delizardo.com>
2021-04-05 14:29:25 -0500

    Reinstalled ESLint; linted calculator.js

--- Lesson02/calculator/calculator.js
+++ Lesson02/calculator/calculator.js
@@ -6,21 +6,19 @@ let quit = 'N';
 do {
   calculate();
   quit = readline.question("Would you like to calculate again? (Y/N): ");
-} while(quit.toUpperCase() != 'N');
+} while (quit.toUpperCase() !== 'N');
 console.log("Thanks for calculating; goodbye!");
 
 function calculate() {
   let calculator = {
-                    x: 0,
-                    y: 0,
-                    '+': function() { return this.x + this.y; },
-                    '-': function() { return this.x - this.y; },
-                    '*': function() { return this.x * this.y; },
-                    '/': function() { return (this.y != 0) ?
-                                              this.x / this.y
-                                              : "Division by zero not allowed!";
-                                    }
-                   }
+    'x': 0,
+    'y': 0,
+    '+': function() { return this.x + this.y },
+    '-': function() { return this.x - this.y },
+    '*': function() { return this.x * this.y },
+    '/': function() { return (this.y !== 0) ? this.x / this.y : "Division by zero not allowed!" }
+  };
+
   calculator['x'] = parseFloat(readline.question('Enter a number: '));
   calculator['y'] = parseFloat(readline.question('Enter a number: '));
   let operation = readline.question('Choose an operation (+, -, *, /): ');

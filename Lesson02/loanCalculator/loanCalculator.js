28f8066cdb07a03f3d9652eee2a96c870bf5dbdd
Del <del@delizardo.com>
2021-04-11 18:38:26 -0500

    wrote getNVal methods for amt, apr, cmpdPer

--- Lesson02/loanCalculator/loanCalculator.js
+++ Lesson02/loanCalculator/loanCalculator.js
@@ -1,3 +1,9 @@
+let readline = require('readline-sync');
+let BACKEND = require('./loanCalcBackend.json');
+let MESSAGES = BACKEND.MESSAGES;
+let ERRORS = BACKEND.ERRORS;
+let QUIT = false;
+
 let loanTerms = {
   amount: 0,
   annualRate: 0,
@@ -24,49 +30,206 @@ let calcAmortization = (loan) => {
   payment /= (Math.pow(1 + loan.ratePerPayment, loan.totalPayments) - 1);
   return payment;
 };
-//greet user: "Welcome to the Loanly Calculator! I'll help you navigate your future loan. Please be sure to read the prompts carefully before responding, and try to have fun! You can press 'Q' at any time to quit. Ready? (Press 'Y' to continue)."
-//Prompt for 'Y'.
-//"Great! Let's get started by asking a few simple questions. First, how much would you like to borrow?"
 
-//"Loan amount: " <== getAndValidateLoanAmount() from user
-  //strip any commas or sigils
-  //convert to number
-  //validate input
-  //"Okay, got it. Just to be sure, you would like to borrow the following amount (Y to confirm, N to reenter): "
-  //IF No, log "Alright, let's try again", clear console and loop back to top of getLoanAmount()
-  //ELSE store input in loanData.amount
+let isYnq = (input) => {
+  if (input === 'Y' || input === 'N' || input === 'Q') {
+    return true;
+  } else {
+    console.log(ERRORS.YNQ);
+    return false;
+  }
+};
 
-//"Let's move on to interest. What's your APR? Give it as a percentage without the '5' (i.e. '5' instead of '0.05' for 5%)."
-//"APR: " <== getAndValidateAPR() from user
-  //strip % if necessary
-  //convert to number
-  //validate input
-  //"Let's double check. Your APR for this loan will be " + input."
-  //"Is that correct? (Y to confirm, N to reenter): "
-  //SET loanData.annualRate = input
+let ynq = () => {
+  let input;
+  do {
+    input = readline.question(MESSAGES.YNQ);
+    input = input.trimStart().toUpperCase();
+  } while (!isYnq(input));
 
-//"Will this loan be compounded monthly? Y for 'yes', N for 'no; the default is 'yes'"
-//IF Y
-  //SET loanData.compoundingPeriodsPerYear = 12
-//ELSE
-  //"Okay, here are some other compounding options. Select the number that corresponds to your loan:"
-  //1) Annually = 1
-  //2) Semi-annually = 2
-  //3) Quarterly = 4
-  //4) Semi-monthly = 6
-  //5) Biweekly = 26
-  //6) Weekly = 52
-  //7) Daily = 365
-  //8) Other
-  //getAndValidateCompChoice()
-  //IF user chooses 8
-    //"Enter the number of compouding periods per year as a whole number: "
-    //validate input
-    //SET loanData.compoundingPeriodsPerYear = input
-  //ELSE
-    //SET loanData.compoundingPeriodsPerYear = compoundingPeriods[choice]
-//"Whew; almost done! Let's be sure we got this right: Your loan should be compounded "+loanTerms.compoundingPeriodsPerYear+" per year."
-//"Y to confirm, N to reenter: "
+  switch (input) {
+    case 'Y':
+      return true;
+    case 'N':
+      return false;
+    case 'Q':
+      QUIT = true;
+      return false;
+    default:
+      console.log(ERRORS.YNQ);
+      return ynq();
+  }
+};
+
+let formatNumber = (num) => {
+  num = num.split(',').join('').split(' ').join('');
+  num = (num.trimStart() === '') ? NaN : parseFloat(num);
+  return num;
+};
+
+let isValidLoanAmt = (num) => {
+  if (Number.isNaN(num)) {
+    console.log(ERRORS.NAN);
+    return false;
+  } else if (num < 0) {
+    console.log(ERRORS.NEG_LOAN_AMT);
+    return false;
+  } else {
+    return true;
+  }
+};
+
+let confirmLoanAmount = (amount) => {
+  console.log(MESSAGES.CONFIRM_LOAN_AMOUNT + amount);
+  return ynq();
+};
+
+let getAndValidateLoanAmount = () => {
+  if (QUIT) return undefined;
+  let input;
+  do {
+    input = readline.question(MESSAGES.REQUEST_LOAN_AMOUNT);
+    input = formatNumber(input);
+  } while (!isValidLoanAmt(input));
+
+  if (confirmLoanAmount(input)) {
+    return input;
+  } else {
+    return getAndValidateLoanAmount();
+  }
+};
+
+let isValidAPR = (num) => {
+  if (Number.isNaN(num)) {
+    console.log(ERRORS.NAN);
+    return false;
+  } else if (num < 0) {
+    console.log(ERRORS.NEG_APR);
+    return false;
+  } else {
+    return true;
+  }
+};
+
+let confirmAPR = (percent) => {
+  console.log(MESSAGES.CONFIRM_APR + percent + "%");
+  return ynq();
+};
+
+let getAndValidateAPR = () => {
+  if (QUIT) return undefined;
+  let input;
+  do {
+    input = readline.question(MESSAGES.REQUEST_APR);
+    input = formatNumber(input);
+  } while (!isValidAPR(input));
+
+  if (confirmAPR(input)) {
+    return input / 100;
+  } else {
+    return getAndValidateAPR();
+  }
+};
+
+let isValidCompounding = (num) => {
+  if (Number.isNaN(num)) {
+    console.log(ERRORS.NAN);
+    return false;
+  } else if (num <= 0) {
+    console.log(ERRORS.INVALID_COMPOUNDING);
+    return false;
+  } else {
+    return true;
+  }
+};
+
+let confirmCompounding = (compoundings) => {
+  console.log(MESSAGES.CONFIRM_COMPOUNDING + compoundings + " times per year.");
+  return ynq();
+};
+
+// eslint-disable-next-line max-lines-per-function
+let getCustomCompounding = () => {
+  if (QUIT) return undefined;
+  let selection = readline.question(MESSAGES.SELECT_COMPOUNDING);
+  let compounding;
+  switch (selection) {
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+      compounding = BACKEND.COMPOUNDING_PERIODS[Number(selection)];
+      if (confirmCompounding(compounding)) {
+        return compounding;
+      } else {
+        return getCustomCompounding();
+      }
+    case '8':
+      do {
+        compounding = readline.question(MESSAGES.GET_CUSTOM_COMPOUNDING);
+        compounding = formatNumber(compounding);
+      } while (!isValidCompounding(compounding));
+      if (confirmCompounding(compounding)) {
+        return compounding;
+      } else {
+        return getCustomCompounding();
+      }
+    default:
+      console.log(ERRORS.INVALID_SELECTION);
+      return getCustomCompounding();
+  }
+};
+
+// eslint-disable-next-line max-lines-per-function
+let getAndValidateCompounding = () => {
+  if (QUIT) return undefined;
+  let selection = readline.question(MESSAGES.START_COMPOUNDING);
+  selection = selection.toUpperCase();
+  switch (selection) {
+    case 'Y':
+      if (confirmCompounding(BACKEND.COMPOUNDING_PERIODS[0])) {
+        return BACKEND.COMPOUNDING_PERIODS[0];
+      } else {
+        return getAndValidateCompounding();
+      }
+    case 'N':
+      return getCustomCompounding();
+    case 'Q':
+      QUIT = true;
+      return undefined;
+    default:
+      console.log(ERRORS.YNQ);
+      return getAndValidateAPR();
+  }
+};
+
+//=================================================================PROGRAM START
+console.log(MESSAGES.GREET);
+if (ynq()) {
+  do {
+    if (!QUIT) {
+      console.log(MESSAGES.START_LOAN_AMOUNT);
+      loanTerms.amount = getAndValidateLoanAmount();
+    }
+    if (!QUIT) {
+      console.log(MESSAGES.START_APR);
+      loanTerms.annualRate = getAndValidateAPR();
+    }
+    if (!QUIT) {
+      loanTerms.compoundingPeriodsPerYear = getAndValidateCompounding();
+    }
+    if (!QUIT) {
+      console.log(MESSAGES.NEW_CALCULATION);
+    }
+  } while (QUIT !== true && ynq() !== false);
+}
+console.log(MESSAGES.GOODBYE);
+//===================================================================PROGRAM END
+
+//TODO: Write functions to get loan duration
 
 //"Last question: How long will you be repaying this loan?"
 //"Years: "

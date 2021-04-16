cc7e96ee784b263b59e1db6fa44ad91465cef589
Del <del@delizardo.com>
2021-04-16 06:35:32 -0500

    added some structure

--- Lesson02/rps/rps.js
+++ Lesson02/rps/rps.js
@@ -1,4 +1,32 @@
-const ART = require('./art.json')
-const ROCK = ART.ROCK;
-const PAPER = ART.PAPER;
-const SCISSORS = ART.SCISSORS;
+let readline = require('readline-sync');
+
+const BACKEND = require('./rpsBackend.json');
+const MESSAGES = BACKEND.MESSAGES;
+const OUTCOMES = BACKEND.OUTCOMES;
+const ERRORS = BACKEND.ERRORS;
+const ROCK = 'R';
+const PAPER = 'P';
+const SCISSORS = 'S';
+
+function isValidChoice(choice) {
+  if (choice === ROCK || choice === PAPER || choice === SCISSORS) {
+    return true;
+  } else {
+    console.log(ERRORS.INVALID_CHOICE);
+    return false;
+  }
+}
+
+function getAndValidateUserChoice() {
+  let input;
+  do {
+    input = readline.question(MESSAGES.PLAYER_CHOICE);
+    input = input.toUpperCase();
+  } while (!isValidChoice(input));
+
+  return input;
+}
+
+function opponentChoice() {
+  //TODO: generate a random number between 0 and 2 inclusive
+}

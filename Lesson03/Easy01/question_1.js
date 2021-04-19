4a89597d1af57c49cfb758201477c8a6563c901e
Del-on-git <del@delizardo.com>
2021-04-19 14:04:50 -0500

    improved Easy1 Q1

--- Lesson03/Easy01/question_1.js
+++ Lesson03/Easy01/question_1.js
@@ -11,5 +11,10 @@ numbers[4];  // what will this line return?
  * numbers[4] => undefined
  *
  * Solution: numbers[6] = 5 actually returns 5, but extends numbers and leaves
- * <3 empty items> in indices 4-5. numbers[4] does return undefined.
+ * <3 empty items> in indices 4-5. numbers[4] does return undefined, HOWEVER it
+ * is actually empty, not undefined. Note the difference:
+ * 
+ * [1, 2, 3, <3 empty items>].map((num) => 10 * num) returns [10, 20, 30, <3 empty itmes>]
+ * 
+ * [1, 2, 3, undefined, undefined, undefined].map((num) => 10 * num) returns [10, 20, 30, NaN, NaN, NaN]
  */

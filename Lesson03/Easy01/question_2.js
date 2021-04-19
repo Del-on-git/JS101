93a09d296080b26dd094c12c6924b8b9ecf42998
Del-on-git <del@delizardo.com>
2021-04-19 14:22:53 -0500

    Solved Easy1 Q2

--- Lesson03/Easy01/question_2.js
+++ Lesson03/Easy01/question_2.js
@@ -7,5 +7,5 @@ let endsWithBang2 = (str) => str.split('').reverse()[0] === '!';
 let readline = require('readline-sync');
 
 let test = readline.question("Give a string: ");
-console.log(`EWB1: ${test}`);
-console.log(`EWB2: ${test}`);
+console.log(`EWB1: ${endsWithBang1(test)}`);
+console.log(`EWB2: ${endsWithBang2(test)}`);

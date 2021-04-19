bf28ed79403c91226355b8cb686b1a0c42d26718
Del-on-git <del@delizardo.com>
2021-04-19 14:24:55 -0500

    Got real answer

--- Lesson03/Easy01/question_2.js
+++ Lesson03/Easy01/question_2.js
@@ -9,3 +9,5 @@ let readline = require('readline-sync');
 let test = readline.question("Give a string: ");
 console.log(`EWB1: ${endsWithBang1(test)}`);
 console.log(`EWB2: ${endsWithBang2(test)}`);
+
+// ANSWER: you could just use String.prototype.endsWith('!');

06f98026fbf3f161450246a4c85e9dd4418b7059
Del-on-git <del@delizardo.com>
2021-04-19 14:19:41 -0500

    Easy 1 Q 2

---
+++ Lesson03/Easy01/question_2.js
@@ -0,0 +1,11 @@
+//How can you determine whether a given string ends with an exclamation mark (!)?
+
+let endsWithBang1 = (str) => str.slice(-1) === '!';
+
+let endsWithBang2 = (str) => str.split('').reverse()[0] === '!';
+
+let readline = require('readline-sync');
+
+let test = readline.question("Give a string: ");
+console.log(`EWB1: ${test}`);
+console.log(`EWB2: ${test}`);

adce0500a37ade88a08e9ccf8b1fa01e52b3b534
Del <del@delizardo.com>
2021-04-20 20:33:21 -0500

    Easy02 Q4

---
+++ Lesson03/Easy02/question_04.js
@@ -0,0 +1,7 @@
+//show two different ways to put the expected "Four score and " in front of
+//the following string
+
+let famousWords = "seven years ago...";
+
+console.log("Four score and " + famousWords);
+console.log("Four score and ".concat(famousWords));

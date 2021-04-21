f42bd3f09b318123a337eb47403347cbcd6bc163
Del <del@delizardo.com>
2021-04-20 20:34:54 -0500

    Easy02 Q10

---
+++ Lesson03/Easy02/question_10.js
@@ -0,0 +1,14 @@
+//Write two one-line expressions to count the number of lower-case t characters
+//in each of the following strings:
+
+//Write two one-line expressions to count the number of lower-case t characters
+//in each of the following strings:
+
+let statement1 = "The Flintstones Rock!";
+let statement2 = "Easy come, easy go.";
+
+console.log(statement1.split('').filter((char) => char === 't').length);
+console.log(statement2.split('').filter((char) => char === 't').length);
+
+console.log(statement1.split('').map((ele) => (ele === 't' ? ele : [])).flat().length);
+console.log(statement2.split('').map((ele) => (ele === 't' ? ele : [])).flat().length);

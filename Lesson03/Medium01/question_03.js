1438a8d3156e098574e017770ba6f2498c119982
Del <del@delizardo.com>
2021-04-21 18:40:57 -0500

    Medium01 Q3

---
+++ Lesson03/Medium01/question_03.js
@@ -0,0 +1,36 @@
+/*
+Improve the following function so that it handles non-positive inputs:
+
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  do {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  } while (divisor !== 0);
+  return factors;
+}
+
+*/
+
+function factors(number) {
+  if (number <= 0) {
+    return [];
+  } else if (number === 1) {
+    return [1];
+  }
+  let divisor = 2;
+  let factors = [1, number];
+  while (divisor < number) {
+    if (number % divisor === 0 && !factors.includes(divisor)) {
+      factors = [divisor, number / divisor, ...factors];
+    }
+    divisor++;
+  }
+
+  return factors.sort((a, b) => a - b);
+}
+
+console.log(factors(330));

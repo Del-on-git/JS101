c30df31c4d82c51594529cc83c9ba87690233a47
Del-on-git <del@delizardo.com>
2021-04-29 14:06:03 -0500

    example 5 improved wording

--- Lesson05/higherOrderFunctions/Explain_This_Code_04.js
+++ Lesson05/higherOrderFunctions/Explain_This_Code_04.js
@@ -6,9 +6,9 @@
   The method `map` is called on the outer array of the literal
   `[[1, 2], [3, 4]]`. The parameter `arr` is iteratively assigned to each
   sub-array, which is then passed to the callback function defined in the
-  outer call of `map`, which will explicitly return the new array returned
+  outer call of `map`, which will explicitly return the new array created
   by the inner call of `map`. The inner call of `map` iteratively assigns the
-  contents of the array passed as `arr` to the parameter `num` and then
+  contents of `arr` to the parameter `num` and then
   implicitly returns the value of the `num` argument multiplied by 2. Each of
   the return values for the inner call to `map` is placed into the array
   explicitly returned by the outercall. The object returned by the outer

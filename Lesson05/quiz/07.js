9d1fe6fccb5e65115eb2d6ef0588125c3c5a4957
Dean Elizardo <del@delizardo.com>
2021-05-01 12:19:08 -0500

    fixed EO

---
+++ Lesson05/quiz/07.js
@@ -0,0 +1,16 @@
+//Explain this code. What's the return value of the final line?
+
+function evenValues(array) {
+  let evens = [];
+
+  array.forEach(value => {
+    if (value % 2 === 0) {
+      evens.push(value);
+    }
+    array.shift();
+  });
+
+  return evens;
+}
+
+evenValues([1, 3, 4, 2, 4, 6, 5, 7, 9, 10, 12]); //=> [4, 4, 12]

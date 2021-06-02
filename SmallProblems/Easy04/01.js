915febb25b4ee111d79755cce364f59eff07e04b
Del <del@delizardo.com>
2021-06-01 21:04:22 -0500

    Easy 04 initial commit; problem 1

---
+++ SmallProblems/Easy04/01.js
@@ -0,0 +1,8 @@
+/*
+Build a program that randomly generates Teddy's age, and logs it to the console.
+Have the age be a random number between 20 and 120 (inclusive).
+*/
+
+let age = 20 + Math.floor(Math.random() * 100);
+
+console.log(`Teddy is ${age} years old!`);
